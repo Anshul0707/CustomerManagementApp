2ed0cf63864d18489e707ab2fdf5853534cbe391
Anshul0707 <anshul.markwade786@gmail.com>
2023-08-07 12:13:24 +0530

    code update

--- src/main/java/com/customer/management/controller/AuthController.java
+++ src/main/java/com/customer/management/controller/AuthController.java
@@ -2,10 +2,10 @@ package com.customer.management.controller;
 
 import com.customer.management.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 
 @RestController
 @RequestMapping("/auth")
@@ -14,17 +14,22 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
-    @GetMapping("/login")
-    public ResponseEntity<String> login() {
-        String token = authService.authenticateAndGetToken();
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestParam String loginId, @RequestParam String password) {
+        try {
+            String token = authService.authenticateAndGetToken(loginId, password);
 
-        if (token != null) {
-            return ResponseEntity.ok(token);
-        } else {
-            return ResponseEntity.badRequest().body("Authentication failed.");
+            if (token != null) {
+                return ResponseEntity.ok(token);
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Authentication failed.");
+            }
+        } catch (HttpClientErrorException e) {
+            return ResponseEntity.status(e.getStatusCode()).body(e.getResponseBodyAsString());
         }
     }
 }
+
 
 
 
